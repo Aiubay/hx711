2de48149c5dcc02875d26bfaba5ae9c39bb94953
aiubay <darkleon25@gmail.com>
2019-07-11 01:08:17 +0700

    add 2nd weight

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ if not EMULATE_HX711:
     print "From HX711"
     import RPi.GPIO as GPIO
     from hx711 import HX711
+    # from hx711 import HX7112
 else:
     from emulated_hx711 import HX711
 
@@ -36,7 +37,7 @@ def cleanAndExit():
     sys.exit()
 
 hx = HX711(5, 6) #GPIONumbering
-
+# h2x = HX7112(20,21)
 # I've found out that, for some reason, the order of the bytes is not always the same between versions of python, numpy and the hx711 itself.
 # Still need to figure out why does it change.
 # If you're experiencing super random values, change these values to MSB or LSB until to get more stable values.
@@ -45,7 +46,7 @@ hx = HX711(5, 6) #GPIONumbering
 # The second paramter is the order of the bits inside each byte.
 # According to the HX711 Datasheet, the second parameter is MSB so you shouldn't need to modify it.
 hx.set_reading_format("MSB", "MSB")
-
+# h2x.set_reading_format("MSB", "MSB")
 # HOW TO CALCULATE THE REFFERENCE UNIT
 # To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
 # In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
@@ -53,16 +54,16 @@ hx.set_reading_format("MSB", "MSB")
 # If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
 #hx.set_reference_unit(113)
 hx.set_reference_unit(92) #calibration unit
-
+# h2x.set_reference_unit(92)
 hx.reset()
-
-hx.tare()
-
+# h2x.reset()
+#hx.tare()
+# h2x.tare()
 print "Tare done! Add weight now..."
 
 # to use both channels, you'll need to tare them both
-#hx.tare_A()
-#hx.tare_B()
+hx.tare_A()
+hx.tare_B()
 
 while True:
     try:
@@ -80,24 +81,35 @@ while True:
 
         # To get weight from both channels (if you have load cells hooked up 
         # to both channel A and B), do something like this
+
+
+        # y = int(60000) #Asumsi berat minimum truck
+        # val_A = hx.getweight(5) #Data dari sensor 1
+        # val_B = h2x.getweight(5) #Data dari sensor 2
+        # print "A: %s  B: %s" % ( val_A, val_B )
+        # if val_A and val_B >= y:
+        #     sql = "INSERT INTO weight (Date,BeratWim1,BeratWim2) VALUES (%s,%s,%s)"
+        #     val =(x,val_A,val_B)
+        #     mycursor.execute(sql,val)
+        #     mydb.commit()
+
+
         val_A = hx.get_weight_A(5)
         val_B = hx.get_weight_B(5)
         print "A: %s  B: %s" % ( val_A, val_B )
-        # if val_A >= 50000
-#     sql = "INSERT INTO weight (Date,BeratWim1,BeratWim2) VALUES (%s,%s,%s)"
-#     val =(x,val_A,val_B)
-#     mycursor.execute(sql,val)
-        sql = "INSERT INTO weight (Date, BeratWim1, BeratWim2) VALUES (%s,%s, %s)"
-        val = (x, val_A, val_B)
-        mycursor.execute(sql, val)
+        if val_A and val_B >= y:
+            sql = "INSERT INTO weight (Date,BeratWim1,BeratWim2) VALUES (%s,%s,%s)"
+            val =(x,val_A,val_B)
+            mycursor.execute(sql,val)
+            mydb.commit()
         
-        mydb.commit()
+       
         
 
         hx.power_down()
         hx.power_up()
         time.sleep(0.1)
-
+        print (mycursor.rowcount, "Data Send to database")
     except (KeyboardInterrupt, SystemExit):
         cleanAndExit()
 print(mycursor.rowcount, "record inserted.")
@@ -106,6 +118,4 @@ print(mycursor.rowcount, "record inserted.")
 #     threading.Timer(600,sendDataToServer).start()
 #     print('Sending data to server ')
 #     print val
-
-
-#     
+ 
