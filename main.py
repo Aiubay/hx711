eea4abd4ff40b11ce9722dd2ea296a0a3eafa085
Aiubay <35898043+aiubay@users.noreply.github.com>
2019-07-16 02:02:00 +0700

    Fixing Gpio numbering

--- main.py
+++ main.py
@@ -36,60 +36,37 @@ def cleanAndExit():
     print "Bye!"
     sys.exit()
 
-hx = HX711(5, 6) #GPIONumbering
-hx2 = HX711(20,21)
-# I've found out that, for some reason, the order of the bytes is not always the same between versions of python, numpy and the hx711 itself.
-# Still need to figure out why does it change.
-# If you're experiencing super random values, change these values to MSB or LSB until to get more stable values.
-# There is some code below to debug and log the order of the bits and the bytes.
-# The first parameter is the order in which the bytes are used to build the "long" value.
-# The second paramter is the order of the bits inside each byte.
-# According to the HX711 Datasheet, the second parameter is MSB so you shouldn't need to modify it.
-hx.set_reading_format("LSB", "MSB")
+hx = HX711(20, 21) #GPIONumbering
+hx2 = HX711(23,24)
+# Pada beberapa versi python,numpy dan hx711 sendiri selalu byte yang tidak selalu sama
+# Jika Anda mengalami nilai-nilai super acak, ubah nilai-nilai ini ke MSB atau LSB hingga mendapatkan nilai yang lebih stabil.
+# Menurut pada Datasheet Hx711, Parameter kedua merupakan MSB JANGAN DI UBAH.
+hx.set_reading_format("MSB", "MSB")
 hx2.set_reading_format("MSB", "MSB")
 # HOW TO CALCULATE THE REFFERENCE UNIT
 # To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
-# In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
-# and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
-# If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
+# Dalam hal ini, 92 adalah 1 gram karena, dengan 1 sebagai unit referensi saya mendapat angka mendekati 0 tanpa bobot
+# dan saya mendapat angka sekitar 184000 ketika saya menambahkan 2kg. Jadi :
+# Jika 2000 gram adalah 184000 maka 1000 gram adalah 184000/2000 = 92.
 #hx.set_reference_unit(113)
-hx.set_reference_unit(1) #calibration unit
-hx2.set_reference_unit(1)
+hx.set_reference_unit(200) #calibration unit
+hx2.set_reference_unit(225)
 hx.reset()
 hx2.reset()
 hx.tare()
 hx2.tare()
 print "Tare done! Add weight now..."
 
-# to use both channels, you'll need to tare them both
-#hx.tare_A()
-#hx.tare_B()
-
 while True:
     try:
-        # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
-        # for the first parameter of "hx.set_reading_format("LSB", "MSB")".
-        # Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment these three lines to see what it prints.
-        
-        # np_arr8_string = hx.get_np_arr8_string()
-        # binary_string = hx.get_binary_string()
-        # print binary_string + " " + np_arr8_string
-        
-        # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
-        # weight = hx.get_weight(5)
-        # print weight
-
-        # To get weight from both channels (if you have load cells hooked up 
-        # to both channel A and B), do something like this
-
-        y = int(50000)  # Asumsi berat minimum truck
+        y = int(1000)  # Asumsi berat minimum truck dalam KG
         jarak = int(20)  # Dalam Meter
 
         Sensor1 = hx.get_weight(5)
         Sensor2 = hx2.get_weight(5)
-        print "A: %s  B: %s" % (Sensor1, Sensor2)
+        print "A: %s Gram, B: %s Gram" % (Sensor1, Sensor2)
 
-        pass
+
         if Sensor1 >= y:
             timeSensor1 = time.time()
             print timeSensor1
@@ -107,17 +84,7 @@ while True:
             mycursor.execute(sql,val)
             mydb.Commit()
             #print (mycursor.rowcount, "Data Send to database")
-            pass
 
-
-        # val_A = hx.get_weight_A(5)
-        # val_B = hx.get_weight_B(5)
-        # print "A: %s  B: %s" % ( val_A, val_B )
-        # if val_A and val_B >= y:
-        #     sql = "INSERT INTO weight (Date,BeratWim1,BeratWim2) VALUES (%s,%s,%s)"
-        #     val =(x,val_A,val_B)
-        #     mycursor.execute(sql,val)
-        #     mydb.commit()
         
        
         
@@ -130,10 +97,6 @@ while True:
         print ""
     except (KeyboardInterrupt, SystemExit):
         cleanAndExit()
-print(mycursor.rowcount, "record inserted.")
-# def sendDataToServer():
+#print(mycursor.rowcount, "record inserted.")
 
-#     threading.Timer(600,sendDataToServer).start()
-#     print('Sending data to server ')
-#     print val
  
